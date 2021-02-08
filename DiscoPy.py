4bc23acfbd4b24906ee9a31276140bf1237f776d
njensen-git <55716571+njensen-git@users.noreply.github.com>
2021-02-08 15:37:14 -0700

    Update DiscoPy.py

--- DiscoPy.py
+++ DiscoPy.py
@@ -26,17 +26,14 @@ async def on_message(message):
 
 
     if message.content.startswith('$loreme'):
-        name = message.content
-        splitname = namecheck(name)
-        champURL = "http://ddragon.leagueoflegends.com/cdn/{}/data/en_US/champion/{}.json"
-        champName = splitname[1]
-        getVersion = requests.get("https://ddragon.leagueoflegends.com/realms/na.json")
-        ddVersion = getVersion.json()['dd']
-        response = requests.get(champURL.format(ddVersion,champName))
+        info = message.content
+        infoList = namecheck(info)
+        champName = infoList[0]
+        champInfo = getChampInfo(champName)
         print(champName)
+        print(champInfo.json()['data'])
         try:
-            
-            data = response.json()
+            data = champInfo.json()
             await message.channel.send(data['data'][champName]['lore'])
         except:
             await message.channel.send("Didn't find that.")
@@ -44,35 +41,73 @@ async def on_message(message):
     #get the ability info
     #format is $skills name(if its sol xin or j4 concatenates the names) abilitykey
     if message.content.startswith('$skills'):
-        name = message.content
-        splitname = namecheck(name)
-        direction = 'C:/Users/jense/Documents/DataDragon/11.1.1/data/en_US/champion/{}.json'
+        info = message.content
+        #list of gained info in form of [name,ability]
+        infoList = namecheck(info)
+        champName = infoList[0]
+        champInfo = getChampInfo(champName)
+        abilityName = infoList[1]
+
         ability = {
             'q': 0,
             'w': 1,
             'e': 2,
             'r': 3
         }
-        whichspell = ability[splitname[2]]
-        #couple test prints for relevant variables
-        # print(whichspell)
-        # print(direction.format(splitname[1]))
-        # print(splitname[1])
         try:
-            with open(direction.format(splitname[1])) as f:
-                data = json.load(f)
-                await message.channel.send(data['data'][splitname[1]]['spells'][whichspell]['cooldown'])
+            whichspell = ability[abilityName]
+        except:
+            await message.channel.send("Not an ability?")
+        try:
+            data = champInfo.json()
+            skillNameData = str(data['data'][champName]['spells'][whichspell]['name'])
+            cdData = str(data['data'][champName]['spells'][whichspell]['cooldown'])
+            costData =  str(data['data'][champName]['spells'][whichspell]['cost'])
+            rangeData =  str(data['data'][champName]['spells'][whichspell]['range'])
+            await message.channel.send(skillNameData + "\nCD: " + cdData + "\nCost: " + costData + "\nRange: " + rangeData)
         except:
             await message.channel.send("Didn't find that.")
 
 def namecheck(name):
     splitname = name.lower().split(" ")
-    splitname[1] = splitname[1].capitalize()
-    if(splitname[1] == 'Aurelion' or splitname[1] == 'Jarvan' or splitname[1] == 'Xin'):
-        splitname[1] = splitname[1] + splitname.pop(2).capitalize()
+    splitname.pop(0)
+    splitname[0] = splitname[0].capitalize()
+    if(splitname[0] == 'Aurelion' or splitname[0] == 'Jarvan'):
+        splitname[0] = splitname[0] + splitname.pop(1).capitalize()
+    if(splitname[0] == 'Twisted' or splitname[0] == 'Xin'):
+        splitname[0] = splitname[0] + splitname.pop(1).capitalize()  
+    if(splitname[0] == 'Tahm' or splitname[0] == 'Miss'):
+        splitname[0] = splitname[0] + splitname.pop(1).capitalize()  
+    if(splitname[0] == 'Master' or splitname[0] == 'Lee'):
+        splitname[0] = splitname[0] + splitname.pop(1).capitalize()  
+    if(splitname[0] == 'Dr'):
+        splitname[0] = splitname[0] + splitname.pop(1).capitalize()  
     #why is it like this?
-    if(splitname[1] == 'Wukong'):
-        splitname[1] = 'MonkeyKing'
+    if(splitname[0] == 'Wukong'):
+        splitname[0] = 'MonkeyKing'
+    #nobody uses their real names
+    if(splitname[0] == 'Tf'):
+        splitname[0] = 'TwistedFate'
+    if(splitname[0] == 'Yi'):
+        splitname[0] = 'MasterYi'
+    if(splitname[0] == 'Kog'):
+        splitname[0] = 'KogMaw'
+    if(splitname[0] == 'Kogmaw'):
+        splitname[0] = 'KogMaw'
+    if(splitname[0] == 'Mundo'):
+        splitname[0] = 'DrMundo'
+    if(splitname[0] == 'Cass'):
+        splitname[0] = 'Cassiopeia'
+    if(splitname[0] == 'Cho'):
+        splitname[0] = 'Chogath'
     return splitname
+
+def getChampInfo(name):
+    champURL = "http://ddragon.leagueoflegends.com/cdn/{}/data/en_US/champion/{}.json"
+    champName = name
+    getVersion = requests.get("https://ddragon.leagueoflegends.com/realms/na.json")
+    ddVersion = getVersion.json()['dd']
+    response = requests.get(champURL.format(ddVersion,champName))
+    return response
 
 client.run('ODA3MDI2Njc3MjAwNzE1Nzg3.YByAKQ.S_dNhcCdFgYFEQAwMNJ8HDLWydU')
